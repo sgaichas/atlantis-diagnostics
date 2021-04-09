fda349cf608b59e78805394b619ed098e851b5cf
andybeet <22455149+andybeet@users.noreply.github.com>
2021-04-09 17:05:59 -0400

    diag_persistence additions - added threshold parameter for persistence option. %age of inital biomass - species argument = NULL to use all species

--- R/diag_persist.R
+++ R/diag_persist.R
@@ -10,9 +10,12 @@
 #'system now, so this test includes historical fishing, physics, and
 #'phytoplankton drivers.
 #'
-#'@param atBtxt A dataframe of total biomass of all groups over time, read in from
+#'@param atBtxt A data frame. Total biomass of all groups over time, read in from
 #'Atlantis ...BioInd.txt output using \code{atlantisom::load_bioind}.
-#'@param fgs.names A vector of species names
+#'@param fgs.names Character vector. A vector of species names in which to test for persistence.
+#'(Default = NULL). For default all species in atBtxt
+#'@param threshold Numeric scalar. Proportion of initial biomass for which for which all species
+#'are measured against. Default = 0, all species need to be non zero.
 #'@param plot A logical value specifying if the function should generate plots or
 #'not. The default is \code{TRUE}.
 #'
@@ -32,24 +35,31 @@
 #'diag_persist(testdiag$atBtxt, testdiag$fgs.names)
 #'}
 #'
-diag_persist <- function(atBtxt, fgs.names, plot=TRUE){
+diag_persist <- function(atBtxt, fgs.names=NULL, threshold = 0, plot=TRUE){
 
   # need in annual units? Or fail when any output timestep below threshold?
   # make safe for migratory species, assume that over the course of the year mean B > 0.
   # assumes biomass never goes negative in atlantis
 
+  if (is.null(fgs.names)) { # select all species
+    fgs.names <- unique(atBtxt$species)
+  }
+
+  # For each species calculate average biomass over each year.
+  # Remember there are multiple time steps per year
+  #
   crash <- atBtxt %>%
-    dplyr::filter(.data$species %in% fgs.names) %>%
-    dplyr::mutate(yr = ceiling(.data$time/365)) %>%
-    dplyr::filter(.data$yr > 0) %>%
-    dplyr::group_by(.data$species, .data$yr) %>%
-    dplyr::summarise(meanB = mean(.data$atoutput)) %>%
-    dplyr::filter(.data$meanB == 0)
+    dplyr::filter(species %in% fgs.names) %>%
+    dplyr::mutate(yr = ceiling(time/365)) %>%
+    dplyr::filter(yr > 0) %>%
+    dplyr::group_by(species, yr) %>%
+    dplyr::summarise(meanB = mean(atoutput)) %>%
+    dplyr::filter(meanB == 0)
 
   # flag any groups with any mean annual biomass below a threshold
 
   crashed <- crash %>%
-    dplyr::group_by(.data$species) %>%
+    dplyr::group_by(species) %>%
     dplyr::count()
 
   names(crashed) <- c("Failed", "nYrs0B")
@@ -62,22 +72,24 @@ diag_persist <- function(atBtxt, fgs.names, plot=TRUE){
                       breaks = c(-Inf, 0, Inf),
                       labels = c("crashed", ">0 B"))
 
-    plotB <-ggplot2::ggplot() +
-      ggplot2::geom_line(data=atBtxt%>%dplyr::filter(.data$species %in% fgs.names),
-                         ggplot2::aes(x=.data$time/365,y=.data$atoutput, color=.data$col),
+    plotB <- ggplot2::ggplot() +
+      ggplot2::geom_line(data=atBtxt %>% dplyr::filter(species %in% fgs.names),
+                         ggplot2::aes(x=time/365,y=atoutput, color=col),
                          alpha = 10/10) +
       ggthemes::theme_tufte() +
-      ggplot2::theme(legend.position = "top") +
-      ggplot2::labs(colour=g.name)
+      ggplot2::theme(legend.position = "top")
+     print(plotB)
+    #+
+    #  ggplot2::labs(colour=g.name)
 
-    print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 1, scales="free"))
-    print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 2, scales="free"))
-    print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 3, scales="free"))
-    print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 4, scales="free"))
-    print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 5, scales="free"))
-    print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 6, scales="free"))
-    print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 7, scales="free"))
-    print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 8, scales="free"))
+     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 1, scales="free"))
+     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 2, scales="free"))
+     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 3, scales="free"))
+     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 4, scales="free"))
+     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 5, scales="free"))
+     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 6, scales="free"))
+     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 7, scales="free"))
+     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 8, scales="free"))
   }
 
   return(as.data.frame(crashed))
