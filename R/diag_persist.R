fde588e4918ccec399c0f01fad8fa3f829659470
andybeet <22455149+andybeet@users.noreply.github.com>
2021-04-12 16:46:21 -0400

    diag_persist function allows user to specify threshold for persistence (based on initial biomass) and number of years to use

--- R/diag_persist.R
+++ R/diag_persist.R
@@ -1,79 +1,112 @@
 #'Test for all functional group persistence
 #'
-#'@description \code{diag_persist} Averages over a year to see if mean B is 0,
-#'assuming that initial (Time = 0) biomass does not count. Output is a list of
-#'species that have crashed and an optional set of plots where 0 biomass is red.
+#' Inspects each time point. If at any point in time a species falls below a predefined floor it
+#' is flagged. The floors are specifies as a proportion of initial biomass. The last n years of the run are
+#' used in the test
 #'
-#'@details This should be run on an unfished, unperturbed run. Fishing or
+#'@param biomass A data frame. Total biomass of all groups over time, read in from
+#'Atlantis ...BioInd.txt output using \code{atlantisom::load_bioind}.
+#'@param speciesNames Character vector. A vector of species names in which to test for persistence.
+#'(Default = NULL, uses all species found in  \code{biomass})
+#'@param nYrs Numeric scalar. Number of years from the end of the time series that persistence must occur.
+#' (Default = NULL, persistence must occur throughout entire time series)
+#'@param floor Numeric scalar. Proportion of initial biomass for which for which all species
+#'are measured against. (Default = 0, all species need to be non zero). Range should be 0-1
+#'@param tol Numeric scalar. Tolerance level to add to biomass floor (Default = 1E-6)
+#'@param plot A logical value specifying if the function should generate plots or
+#'not. (Default = F).
+#'
+#'@importFrom magrittr %>%
+#'
+#'@section Details:
+#' Need To Edit. Maybe not applicable. This should be run on an unfished, unperturbed run. Fishing or
 #'perturbations may legitimately drive groups extinct. However, for our
 #'historical period, we don’t expect anything to go extinct that is in the
 #'system now, so this test includes historical fishing, physics, and
 #'phytoplankton drivers.
 #'
-#'@param atBtxt A data frame. Total biomass of all groups over time, read in from
-#'Atlantis ...BioInd.txt output using \code{atlantisom::load_bioind}.
-#'@param fgs.names Character vector. A vector of species names in which to test for persistence.
-#'(Default = NULL). For default all species in atBtxt
-#'@param threshold Numeric scalar. Proportion of initial biomass for which for which all species
-#'are measured against. Default = 0, all species need to be non zero.
-#'@param plot A logical value specifying if the function should generate plots or
-#'not. The default is \code{TRUE}.
 #'
-#'@importFrom magrittr %>%
-#'@importFrom rlang .data
+#'@return Returns a data frame of species which do not meet defined persistence criteria.
+
+#'\item{species}{The common name of the species/functional group}
+#'\item{Code}{Atlantis Code for species/functional group}
+#'\item{minimumBiomass}{The smallest value of biomass observed in the run}
+#'\item{tminimumBiomass}{The time step in which \code{minimumBiomass} occurred }
+#'\item{t1}{The first time step persistence was not met}
+#'\item{tn}{The last time step persistence was not met}
+#'\item{nts}{The total number of time steps that persistence was not met}
 #'
-#'@return Returns a dataframe of crashed species with columns "Failed" of species
-#'names and "nYrs0B" with number of years having 0 biomass. Also plots all species
-#'trajectories with red lines for crashed years unless plot=FALSE.
 #'@export
 #'
-#'@author Sarah Gaichas
 #'
 #'@examples
 #'\dontrun{
 #'# Usage
 #'diag_persist(testdiag$atBtxt, testdiag$fgs.names)
 #'}
-#'
-diag_persist <- function(atBtxt, fgs.names=NULL, threshold = 0, plot=TRUE){
+
+diag_persist <- function(biomass, speciesNames=NULL, nYrs = NULL, floor = 0, tol = 1E-6, plot=F){
 
   # need in annual units? Or fail when any output timestep below threshold?
   # make safe for migratory species, assume that over the course of the year mean B > 0.
   # assumes biomass never goes negative in atlantis
 
-  if (is.null(fgs.names)) { # select all species
-    fgs.names <- unique(atBtxt$species)
+  # find final time step value
+  maxRuntime <- max(biomass$time)
+
+  # use default options
+  if (is.null(speciesNames)) { # select all species
+    speciesNames <- unique(biomass$species)
+  }
+  if (is.null(nYrs)) { # use all time series
+    filterTime <- 0
+  } else { # last n years
+    filterTime <- maxRuntime- (365*nYrs)
   }
 
-  # For each species calculate average biomass over each year.
-  # Remember there are multiple time steps per year
-  #
-  crash <- atBtxt %>%
-    dplyr::filter(species %in% fgs.names) %>%
-    dplyr::mutate(yr = ceiling(time/365)) %>%
-    dplyr::filter(yr > 0) %>%
-    dplyr::group_by(species, yr) %>%
-    dplyr::summarise(meanB = mean(atoutput)) %>%
-    dplyr::filter(meanB == 0)
 
-  # flag any groups with any mean annual biomass below a threshold
-
-  crashed <- crash %>%
+  # For each species calculate which time steps biomass is below persistence threshold
+  # we look at biomass < % initial Biomass
+  status <- biomass %>%
+    dplyr::filter(time >= filterTime) %>%
+    dplyr::filter(species %in% speciesNames) %>%
+    dplyr::select(species, time, atoutput) %>%
     dplyr::group_by(species) %>%
-    dplyr::count()
+    dplyr::mutate(proportionInitBio = atoutput/dplyr::first(atoutput)) %>%
+    dplyr::filter(proportionInitBio <= (floor + tol)) %>%
+    dplyr::ungroup()
 
-  names(crashed) <- c("Failed", "nYrs0B")
+  if (nrow(status) == 0) { # all pass species persist at desired levels
+    return(persistence=NULL)
+  }
+
+  # max breach, time of max breach, num breaches
+  persistence <- status %>%
+    dplyr::group_by(species) %>%
+    dplyr::mutate(minimumBiomass = min(atoutput)) %>%
+    dplyr::mutate(nts = dplyr::n()) %>%
+    dplyr::mutate(tminimumBiomass = dplyr::case_when(atoutput == min(atoutput) ~ time)) %>%
+    dplyr::mutate(t1 = min(time), tn = max(time)) %>%
+    dplyr::filter(!is.na(tminimumBiomass)) %>%
+    dplyr::select(species,proportionInitBio, minimumBiomass, tminimumBiomass, t1, tn, nts) %>%
+    dplyr::filter(tminimumBiomass == min(tminimumBiomass))
+
+
+
+  return(persistence)
+
+
 
   # visualize; hardcoded pages for ~89 group NEUS model
 
   if(plot){
 
-    atBtxt$col <- cut(atBtxt$atoutput,
+    atBtxt$col <- cut(biomass$atoutput,
                       breaks = c(-Inf, 0, Inf),
                       labels = c("crashed", ">0 B"))
 
     plotB <- ggplot2::ggplot() +
-      ggplot2::geom_line(data=atBtxt %>% dplyr::filter(species %in% fgs.names),
+      ggplot2::geom_line(data=biomass %>% dplyr::filter(species %in% fgs.names),
                          ggplot2::aes(x=time/365,y=atoutput, color=col),
                          alpha = 10/10) +
       ggthemes::theme_tufte() +
@@ -82,14 +115,14 @@ diag_persist <- function(atBtxt, fgs.names=NULL, threshold = 0, plot=TRUE){
     #+
     #  ggplot2::labs(colour=g.name)
 
-     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 1, scales="free"))
-     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 2, scales="free"))
-     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 3, scales="free"))
-     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 4, scales="free"))
-     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 5, scales="free"))
-     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 6, scales="free"))
-     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 7, scales="free"))
-     print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 8, scales="free"))
+     # print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 1, scales="free"))
+     # print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 2, scales="free"))
+     # print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 3, scales="free"))
+     # print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 4, scales="free"))
+     # print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 5, scales="free"))
+     # print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 6, scales="free"))
+     # print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 7, scales="free"))
+     # print(plotB + ggforce::facet_wrap_paginate(~species, ncol=4, nrow = 3, page = 8, scales="free"))
   }
 
   return(as.data.frame(crashed))
