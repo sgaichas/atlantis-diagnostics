f506e21be05e0b342068e0df5f95bb90829162fd
andybeet <22455149+andybeet@users.noreply.github.com>
2021-05-14 16:07:13 -0400

    changes speciesNames to speciesCodes - allow user to pass Atlantis Code as argument rather than species name - check for invalid codes - add codes to output

--- R/diag_persistence.R
+++ R/diag_persistence.R
@@ -6,8 +6,8 @@
 #'
 #'@param modelBiomass A data frame. Total biomass of all groups over time, read in from
 #'Atlantis ...BioInd.txt output using \code{atlantisom::load_bioind}.
-#'@param speciesNames Character vector. A vector of species names in which to test for persistence.
-#'(Default = NULL, uses all species found in  \code{biomass})
+#'@param speciesCodes Character vector. A vector of Atlantis species codes in which to test for persistence.
+#'(Default = NULL, uses all species found in  \code{modelBiomass})
 #'@param nYrs Numeric scalar. Number of years from the end of the time series that persistence must occur.
 #' (Default = NULL, persistence must occur throughout entire time series)
 #'@param floor Numeric scalar. Proportion of initial biomass for which for which all species
@@ -18,13 +18,6 @@
 #'
 #'@importFrom magrittr %>%
 #'
-#'@section Details:
-#' Need To Edit. Maybe not applicable. This should be run on an unfished, unperturbed run. Fishing or
-#'perturbations may legitimately drive groups extinct. However, for our
-#'historical period, we don’t expect anything to go extinct that is in the
-#'system now, so this test includes historical fishing, physics, and
-#'phytoplankton drivers.
-#'
 #'
 #'@return Returns a data frame of species which do not meet defined persistence criteria.
 #'
@@ -33,6 +26,7 @@
 #'\item{initialBiomass}{Starting value of Biomass for species/functional group}
 #'\item{minimumBiomass}{The smallest value of biomass observed in the run}
 #'\item{tminimumBiomass}{The time step in which \code{minimumBiomass} occurred }
+#'\item{proportionInitBio }{The proportion of initial biomass at \code{tminimumBiomass}}
 #'\item{t1}{The first time step persistence was not met}
 #'\item{tn}{The last time step persistence was not met}
 #'\item{nts}{The total number of time steps that persistence was not met}
@@ -47,18 +41,19 @@
 #' file_fgs <- "neus_groups.csv"
 #' # use atlantisom to read them in
 #' fgs <- atlantisom::load_fgs(inDir,file_fgs)
-#' biomass <- atlantisom::load_bioind(outDir,biol.file,fgs)
+#' modelBiomass <- atlantisom::load_bioind(outDir,biol.file,fgs)
 #'
 #' # find all species that do not have biomass > 0 for any time during the run.
-#' diag_persist(biomass)
+#'
+#' diag_persistence(modelBiomass,speciesCodes=NULL, nYrs = NULL, floor = 0)
 #'
 #' # only evaluate herring. Require stability over the last 10 years of the run and all values should
 #' exceed 10% of initial biomass
-#' diag_persist(biomass, speciesNames="Herring", nYrs = 10, floor = 0.1)
+#' diag_persistence(modlBiomass, speciesCodes="HER", nYrs = 10, floor = 0.1)
 #'
 #'}
 
-diag_persistence <- function(modelBiomass, speciesNames=NULL, nYrs = NULL, floor = 0, tol = 1E-6, plot=F){
+diag_persistence <- function(modelBiomass, speciesCodes=NULL, nYrs = NULL, floor = 0, tol = 1E-6, plot=F){
 
   # need in annual units? Or fail when any output timestep below threshold?
   # make safe for migratory species, assume that over the course of the year mean B > 0.
@@ -66,25 +61,36 @@ diag_persistence <- function(modelBiomass, speciesNames=NULL, nYrs = NULL, floor
 
   # find final time step value
   maxRuntime <- max(modelBiomass$time)
-
+  atlantisCodes <- modelBiomass %>%
+    dplyr::filter(!is.na(code)) %>%
+    dplyr::distinct(code) %>%
+    dplyr::pull()
   # use default options
-  if (is.null(speciesNames)) { # select all species
-    speciesNames <- unique(modelBiomass$species)
+  if (is.null(speciesCodes)) { # select all species
+    speciesCodes <- atlantisCodes
+  } else {
+    # remove NA's check for codes not in atlantis
+    speciesCodes <- speciesCodes[!is.na(speciesCodes)]
+    # check to make sure no non atlantis codes
+    invalidCodes <- base::setdiff(speciesCodes,atlantisCodes)
+
+    if (!(length(invalidCodes)==0)){
+      stop("Invalid Atlantis group codes: ",paste0(invalidCodes,collapse=", "))
+    }
   }
   if (is.null(nYrs)) { # use all time series
     filterTime <- 0
   } else { # last n years
-    filterTime <- maxRuntime- (365*nYrs)
+    filterTime <- maxRuntime - (365*nYrs)
   }
-
 
   # For each species calculate which time steps biomass is below persistence threshold
   # we look at biomass < % initial Biomass
   status <- modelBiomass %>%
     dplyr::filter(time >= filterTime) %>%
-    dplyr::filter(species %in% speciesNames) %>%
-    dplyr::select(species, time, atoutput) %>%
-    dplyr::group_by(species) %>%
+    dplyr::filter(code %in% speciesCodes) %>%
+    dplyr::select(code,species, time, atoutput) %>%
+    dplyr::group_by(code) %>%
     dplyr::mutate(initialBiomass = dplyr::first(atoutput)) %>%
     dplyr::mutate(proportionInitBio = atoutput/initialBiomass) %>%
     dplyr::filter(proportionInitBio <= (floor + tol)) %>%
@@ -97,13 +103,13 @@ diag_persistence <- function(modelBiomass, speciesNames=NULL, nYrs = NULL, floor
   # num times threshold exceeded, when largest exceedance occurs and value of biomass,
   # range of exceedances
   persistence <- status %>%
-    dplyr::group_by(species) %>%
+    dplyr::group_by(code) %>%
     dplyr::mutate(minimumBiomass = min(atoutput)) %>%
     dplyr::mutate(nts = dplyr::n()) %>%
     dplyr::mutate(tminimumBiomass = dplyr::case_when(atoutput == min(atoutput) ~ time)) %>%
     dplyr::mutate(t1 = min(time), tn = max(time)) %>%
     dplyr::filter(!is.na(tminimumBiomass)) %>%
-    dplyr::select(species,initialBiomass,proportionInitBio, minimumBiomass, tminimumBiomass, t1, tn, nts) %>%
+    dplyr::select(code,species,initialBiomass,proportionInitBio, minimumBiomass, tminimumBiomass, t1, tn, nts) %>%
     dplyr::filter(tminimumBiomass == min(tminimumBiomass)) %>%
     dplyr::ungroup()
 
