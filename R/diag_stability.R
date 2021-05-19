7f90d58d5c8e4701ff945e0015d9e259901ca576
andybeet <22455149+andybeet@users.noreply.github.com>
2021-05-19 11:14:40 -0400

    changes to diag_stability - added initialYr as argument - fit annual biomass over time - rename slope to mtperyear - scale slope by initial time point and included as output - add sign of slope as output - sort output

--- R/diag_stability.R
+++ R/diag_stability.R
@@ -7,6 +7,7 @@
 #'
 #'@param modelBiomass A data frame. Total biomass of all groups over time, read in from
 #'Atlantis ...BioInd.txt output using \code{atlantisom::load_bioind}.
+#'@param initialYr Numeric Scalar. Year in which the model run was initiated. (Default = 1964)
 #'@param speciesCodes Character vector. A vector of Atlantis species codes in which to test for stability.
 #'(Default = NULL, uses all species found in  \code{modelBiomass})
 #'@param nYrs Numeric scalar. Number of years from the end of the time series that stability must occur.
@@ -19,7 +20,7 @@
 #'@return Returns a data frame of all species and how they measure up against the stability criterion
 #'\item{code}{Atlantis Code for species/functional group}
 #'\item{species}{The common name of the species/functional group}
-#'\item{slope}{Double. The value of the slope parameter (time) }
+#'\item{mtperyear}{Double. The value of the slope parameter (year) }
 #'\item{pValue}{Double. The p-value associated with the slope parameter (time)}
 #'\item{pass}{Logical. Does the species/group pass the test for stability}
 #'
@@ -31,6 +32,8 @@
 #' \deqn{biomass_t = \mu + \beta.t + \epsilon_t  where \epsilon_t ~ IID N(0,\sigma^2)}
 #'
 #' where null hypothesis,  \deqn{H0:\beta=0}
+#'
+#' Note: annual biomass is used in fitting.
 #'
 #'@export
 #'
@@ -52,40 +55,50 @@
 #' diag_stability(modelBiomass, speciesCodes=c("HER","WHK"), nYrs = 10, sigTest = 0.1)
 #'}
 
-diag_stability <- function(modelBiomass, speciesCodes, nYrs = 20, sigTest = 0.05, plot=F){
-
-  # find final time step value
-  maxRuntime <- max(modelBiomass$time)
+diag_stability <- function(modelBiomass, initialYr = 1964, speciesCodes, nYrs = 20, sigTest = 0.05, plot=F){
 
   # check for valid species Codes & clean
   speciesCodes <- check_species_codes(modelBiomass,speciesCodes)
 
+  # determine time frame in which to perform "test"
+  # filter model biomass and average over the year
+  # join with initial biomass
+  modelBiomass <- modelBiomass %>%
+    dplyr::select(time, atoutput, code, species) %>%
+    dplyr::rename(value=atoutput) %>%
+    dplyr::mutate(yearStep= floor(time/365)) %>%
+    dplyr::group_by(code,yearStep,species) %>%
+    dplyr::summarise(meanBio = mean(value),.groups="drop") %>%
+    dplyr::mutate(year = yearStep+initialYr,yearStep=NULL) %>%
+    dplyr::ungroup()
 
   if (is.null(nYrs)) { # use all time series
-    filterTime <- 0
+    filterTime <- initialYr
   } else { # last n years
-    filterTime <- maxRuntime - (365*nYrs)
+    filterTime <- max(modelBiomass$year) - nYrs
   }
 
-  # select species, timframe, group, nest
+  # select species, time frame, group, nest
   stable <- modelBiomass %>%
-    dplyr::filter(time >= filterTime) %>%
+    dplyr::filter(year > filterTime) %>%
     dplyr::filter(code %in% speciesCodes) %>%
-    dplyr::select(code, species,time,atoutput) %>%
     dplyr::group_by(code,species) %>%
-    tidyr::nest()
+    dplyr::mutate(t1Biomass = dplyr::first(meanBio)) %>%
+    dplyr::group_by(code,species,t1Biomass) %>%
+    tidyr::nest() # produces a column called data
 
-  # fit linear model (biomass = alpha + time.beta) to each species over the time frame specified
+  # fit linear model (biomass = alpha + year.beta) to each species over the time frame specified
   # then extract slope and significance from model fit
   stability <- stable %>%
     dplyr::mutate(model = purrr::map(data,fitlm)) %>%
-    dplyr::transmute(species,slope = purrr::map_dbl(model,coefs),pValue=purrr::map_dbl(model,pVals)) %>%
+    dplyr::transmute(species,mtperyear = purrr::map_dbl(model,coefs),pValue=purrr::map_dbl(model,pVals)) %>%
     dplyr::mutate(pass = pValue > sigTest) %>%
-    dplyr::ungroup()
-
+    dplyr::ungroup() %>%
+    dplyr::mutate(relChange = mtperyear/t1Biomass) %>%
+    dplyr::mutate(sign = base::sign(mtperyear))
 
   stability <- stability %>%
-    dplyr::arrange(pass,pValue)
+    dplyr::arrange(pass,desc(abs(relChange)))
 
   return(stability)
 
@@ -97,15 +110,15 @@ diag_stability <- function(modelBiomass, speciesCodes, nYrs = 20, sigTest = 0.05
 ## Helper functions
 
 coefs <- function(model){
-  coefficients(model)[["time"]]
+  coefficients(model)[["year"]]
 }
 
 pVals <- function(model){
-  summary(model)$coefficients[,4][["time"]]
+  summary(model)$coefficients[,4][["year"]]
 }
 
 fitlm <- function(df){
-  lm(atoutput ~ time, data=df)
+  lm(meanBio ~ year, data=df)
 }
 
 
