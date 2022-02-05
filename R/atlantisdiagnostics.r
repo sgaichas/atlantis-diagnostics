d193209b2b7cf0bd3a7bbdf9d6db79413b71c7bb
andybeet <22455149+andybeet@users.noreply.github.com>
2022-02-04 19:14:20 -0500

    added package documentation - atlantisdiagnostics.r

---
+++ R/atlantisdiagnostics.r
@@ -0,0 +1,22 @@
+#'atlantisdiagnostics: Tools for calibrating Atlantis ecosystem models
+#'
+#'Tools to aid in diagnosing issues in a parameterized Atlantis Ecosystem model
+#'
+#'\itemize{
+#'\item Determine functional group persistence over time
+#'\item Determine functional group stability over time
+#'\item Determine functional group biomass (relative to historic data) lie within reasonable bounds
+#'\item Determine distribution of biomass over age classes
+#'\item Determine maximum size (length and weight) of species relative to observations
+#'
+#'}
+#'
+#'@section References:
+#'
+#'
+#'To learn more about using \code{atlantisdiagnostics}, start with the vignette: \code{browseVignettes(package="atlantisdiagnostics")} or click the index below
+#'
+#'
+#' @docType package
+#' @name atlantisdiagnostics
+NULL
